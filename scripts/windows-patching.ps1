5d9114f092ae4eb78ce5e5e67008c15973835ce9
Nurdeen Darman <35269485+Nurdeen21@users.noreply.github.com>
2025-06-21 16:07:52 -0400

    Update windows-patching.ps1

--- scripts/windows-patching.ps1
+++ scripts/windows-patching.ps1
@@ -1,1 +1,6 @@
+# windows-patching.ps1
+# Description: This script installs Windows updates using PowerShell.
+# Intended for after-hours use on workstations or servers.
+
+Install-WindowsUpdate -AcceptAll -AutoReboot
 
